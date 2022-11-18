bb1c4252bfd182d66f3e5e6e1f35302a9e452134
Nick Jennings <nick@silverbucket.net>
2022-11-18 16:42:37 +0100

    remove storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:storybook/recommended",
+    "prettier"
   ],
   plugins: ["svelte3", "@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
